367c7a241f7519b78bf13b5f2d8c6f279a35aa21
davyson <davyssonlevi2.0@gmail.com>
2026-05-23 15:17:21 -0300

    adicionando as variaveis - terminar dps

---
+++ src/components/FeedbackInput/elements/Estrelas.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function Estrelas(){
+    const [ estrelas, setEstrelas ] = useState(0)
+    const []
+    return(
+        <>
+        <input type="checkbox"  />
+        <input type="checkbox"  />
+        <input type="checkbox"  />
+        <input type="checkbox"  />
+        <input type="checkbox"  />
+        </>
+    )
+}
---
+++ src/components/FeedbackInput/elements/Inputs.jsx
@@ -0,0 +1,10 @@
+export default function Inputs(){
+    return(
+        <>
+        <div>
+            <input type="text" placeholder="Titulo"/>
+            <textarea placeholder="O que podemos melhorar" style={{width:"390px", height: "200px"}}></textarea>
+        </div>
+        </>
+    )
+}
