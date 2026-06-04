cdc58bd08ab26a252e3741661be9fbdaf0c78b71
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-06-04 20:50:53 +0000

    estilizando footer e padronizando fonte

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,22 +2,33 @@ import logoEp from "./../../assets/paginaInicial/patriocinadores/logoEP.png"
 import instagram from "./../../assets/paginaInicial/socialMedias/Instagram_logo.png"
 import tiktok from "./../../assets/paginaInicial/socialMedias/tiktok_logo.png"
 import animeProfile from "./../../assets/paginaInicial/patriocinadores/animeprofile.png"
+import "./footer.css"
 
 export default function Footer(){
     return(
-        <footer className="footer">
+        <footer>
             <div className="criadores">
-                <img src={logoEp} alt="logo da ep"/>
-                <p>Feito por: Segundo desenvolvimentos de Sistemas da <br /> escola EEEP Deputado Roberto Mesquita</p>
+                <img src={logoEp} className="logoEp" alt="logo da ep"/>
+                <p>
+                    Feito por: 2°Desenvolvimentos de Sistemas da <br /> EEEP Deputado Roberto Mesquita
+                </p>
             </div>
             <div className="mediasSociais">
                 <h3>Redes sociais</h3>
-                <a href="https://www.instagram.com/2anodesenv.sistemas/" target="_blank"><img src={instagram} alt="logo do instagram"/></a>
-                <a href="https://www.tiktok.com/@segundo.ds2025" target="_blank"><img src={tiktok} alt="logo do tiktok"/></a>
+                <div className="icones">
+                    <a href="https://www.instagram.com/2anodesenv.sistemas/" target="_blank">
+                        <img src={instagram} className="logoInsta" alt="logo do instagram"/>
+                    </a>
+                    <a href="https://www.tiktok.com/@segundo.ds2025" target="_blank">
+                        <img src={tiktok} className="logoTTK" alt="logo do tiktok"/>
+                    </a>
+                </div>
             </div>
             <div className="patriocinadores">
                 <h3>Patriocinadores</h3>
-                <a href="https://animeprofile.com.br/"><img src={animeProfile} alt="logo do anime profile" /></a>
+                <a href="https://animeprofile.com.br/"  target="_blank">
+                    <img src={animeProfile} className="logoAP" alt="logo do anime profile" />
+                </a>
             </div>
         </footer>
     )
