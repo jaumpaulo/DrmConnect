ee56c6a6b507bb316d93f7b41e2594f79b870144
João Paulo <shidp65@gmail.com>
2026-06-13 20:59:42 +0000

    sidebar totalmente feita

--- src/layout/Sidebar/index.jsx
+++ src/layout/Sidebar/index.jsx
@@ -1,29 +1,58 @@
 import "./style.css"
 import Botao from "../../ui/Botoes"
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Logo from "../../assets/logo/logo.png"
 import { House, Handshake, UserRoundPen, Star, LogOut } from 'lucide-react';
+import { supabase } from "../../supabase/supabase"
 
 export default function Sidebar() {
     const irPara = useNavigate()
-    async function desFazerLogin() {
-        const res = supabase.auth.signOut()
-      }
+    const location = useLocation()
 
-    return(
+    async function desFazerLogin() {
+        await supabase.auth.signOut()
+    }
+
+    return (
         <div className="container">
-            {/* logo */}
             <img src={Logo} alt="Logo" />
 
             <div className="btn-redirecionamentos">
-                <Botao texto={"Comunidade"} icon={House} onClick={() => irPara("/home")} className="btn" />
-                <Botao texto={"Ajuda DRM"} icon={Handshake } onClick={() => irPara("/perguntas")} className="btn" />
-                <Botao texto={"Perfil"} icon={UserRoundPen } onClick={() => irPara("/home/seuUser")} className="btn" />
-                <Botao texto={"Feedback"} icon={Star} onClick={() => irPara("/feedback")} className="btn" />
+                <Botao
+                    texto="Comunidade"
+                    icon={House}
+                    onClick={() => irPara("/home")}
+                    className={location.pathname === "/home" ? "btn ativo" : "btn"}
+                />
+
+                <Botao
+                    texto="Ajuda DRM"
+                    icon={Handshake}
+                    onClick={() => irPara("/perguntas")}
+                    className={location.pathname === "/perguntas" ? "btn ativo" : "btn"}
+                />
+
+                <Botao
+                    texto="Perfil"
+                    icon={UserRoundPen}
+                    onClick={() => irPara("/home/seuUser")}
+                    className={location.pathname === "/home/seuUser" ? "btn ativo" : "btn"}
+                />
+
+                <Botao
+                    texto="Feedback"
+                    icon={Star}
+                    onClick={() => irPara("/feedback")}
+                    className={location.pathname === "/feedback" ? "btn ativo" : "btn"}
+                />
             </div>
 
-            {/* Logout */}  
-            <Botao texto={"Logout"} icon={LogOut} onClick={() => desFazerLogin()} className="btn" />
+            <Botao
+                texto="Logout"
+                icon={LogOut}
+                onClick={() => desFazerLogin()}
+                className="btn"
+            />
         </div>
     )
 }
